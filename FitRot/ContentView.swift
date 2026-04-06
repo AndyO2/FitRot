976802ae41735ffebe1fd08c10464c1a01b81d84
andyokamoto <andy.okamoto@gmail.com>
2026-04-06 12:43:23 -0600

    Initial Commit

---
+++ FitRot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FitRot
+//
+//  Created by Andy Okamoto on 4/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FitRot/FitRotApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitRotApp.swift
+//  FitRot
+//
+//  Created by Andy Okamoto on 4/6/26.
+//
+
+import SwiftUI
+
+@main
+struct FitRotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
